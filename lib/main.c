f877ecbce3f253760fe9449285bbac9a36fd645e
Frank Xu <frankxu1992@gmail.com>
2018-07-09 15:07:22 -0700

    Initial Commit

---
+++ lib/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  lib
+//
+//  Created by Frank Xu on 2018-07-09.
+//  Copyright © 2018 Frank Xu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
